d77c12b72a04e387307da463ce9a0486774a33ce
Gor <7495500@gmail.com>
2023-08-21 18:59:01 +0300

    Clear cargos controller and views

--- app/controllers/cargos_controller.rb
+++ app/controllers/cargos_controller.rb
@@ -5,14 +5,11 @@ class CargosController < ApplicationController
     @cargos = Cargo.all
   end
 
-  def show
-  end
+  def show; end
+  def edit; end
 
   def new
     @cargo = Cargo.new
-  end
-
-  def edit
   end
 
   def update_state
@@ -24,11 +21,9 @@ class CargosController < ApplicationController
 
     respond_to do |format|
       if @cargo.save
-        format.html { redirect_to cargo_url(@cargo), notice: "Cargo was successfully created." }
-        format.json { render :show, status: :created, location: @cargo }
+        format.html { redirect_to cargo_url(@cargo) }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @cargo.errors, status: :unprocessable_entity }
+        format.html { render :new }
       end
     end
   end
@@ -36,26 +31,21 @@ class CargosController < ApplicationController
   def update
     respond_to do |format|
       if @cargo.update(cargo_params)
-        format.html { redirect_to cargo_url(@cargo), notice: "Cargo was successfully updated." }
-        format.json { render :show, status: :ok, location: @cargo }
+        format.html { redirect_to cargo_url(@cargo) }
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @cargo.errors, status: :unprocessable_entity }
+        format.html { render :edit }
       end
     end
   end
 
   def destroy
     @cargo.destroy
-
     respond_to do |format|
-      format.html { redirect_to cargos_url, notice: "Cargo was successfully destroyed." }
-      format.json { head :no_content }
+      format.html { redirect_to cargos_url }
     end
   end
 
   private
-
   def set_cargo
     @cargo = Cargo.find(params[:id])
   end
