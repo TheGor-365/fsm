a777903c0c132cc6af97dec48affe461e5ec2693
Gor <7495500@gmail.com>
2023-08-21 18:53:47 +0300

    Modify cargo.rb

--- app/models/cargo.rb
+++ app/models/cargo.rb
@@ -3,12 +3,12 @@ class Cargo < ApplicationRecord
 
   aasm column: 'delivery_state', no_direct_assignment: true, whiny_transitions: false do
     state :at_store, initial: true
-    state :on_the_way
+    state :on_the_way, after_transition: :notify_cargo_on_the_way
     state :at_customs
     state :delivered
 
     event :send_on do
-      transitions from: :at_store, to: :on_the_way
+      transitions from: :at_store, to: :on_the_way, if: :prepared_for_sending?
     end
 
     event :araived_to_customs do
@@ -18,5 +18,13 @@ class Cargo < ApplicationRecord
     event :deliver do
       transitions from: %w(on_the_way at_customs), to: :delivered
     end
+  end
+
+  def notify_cargo_on_the_way
+    'It is on the way'
+  end
+
+  def prepared_for_sending
+    true
   end
 end
